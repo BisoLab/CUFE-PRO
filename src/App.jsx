80a0930736215926bf1da147fd3ab4c94942c717
biso <basselalaa33@gmail.com>
2026-02-07 22:24:43 +0200

    Some additions for github deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 import HomePage from './HomePage';
 import SchedulePage from './SchedulePage';
